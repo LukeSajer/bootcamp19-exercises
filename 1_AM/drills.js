608a9990faf8aa738d2d40bbee668ad62d660ca1
Luke Sajer <lukesajer@Lukes-MacBook-Pro.local>
2019-01-07 13:04:33 -0500

    Day1AM submit 2

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -23,12 +23,9 @@ const uniqueOnes = arr => { let unique = [new Set(arr)];
 const factorial = num => {
   if (num >=1) 
   {
-    return num *factorial(num-1)}
-  }    
-  else{
-  return 1}
+    return num * factorial(num-1)}
 
-}
+  else return 1}
 console.log(factorial(3))
 
 
