60d51fe9ee5a8c6a67b9170ed73a672b2a2e9ba8
Luke Sajer <luke_sajer@college.harvard.edu>
2019-01-09 12:56:27 -0500

    huzzah we do interwebz

--- 2_PM/placeinfo.js
+++ 2_PM/placeinfo.js
@@ -2,29 +2,26 @@
 / returns the following string, formatted with the appropriate values:
 
 'INPUTTED_PLACE is located at coordinates RETURNED_COORDINATES, at the address RETURNED_ADDRESS.' */
-const gmc = require('./google_api.js');
+const client = require('./google_api.js');
 const config = require('./config.js');
 
 //console.log(config);
 //console.log(googleMapsClient);
 const place_input = process.argv[2];
 
-gmc.findPlace({input: process.argv[2], inputtype: 'textquery'}).asPromise()
+client.findPlace({input: process.argv[2], inputtype: 'textquery'}).asPromise()
   .then((response) => {
-    const {candidates}  = response.json;
-    const place_id = candidates[0].place_id;
-    console.log(place_id);
+    //const {candidates}  = response.json;
+    const placeid = candidates[0].place_id
+    //console.log(place_id);
 
-    gmc.place({ placeid: place_id, con}).asPromise().then((response)) => {
-        console.log('got here');
-        
-      //googleMapsClient.place({placeid: place_id}).asPromise().then((response) =>
-      //console.log(response.json)
-    
+    client.place({placeid}).asPromise().then((response)) => {
+        const {lat, lng} = response.json.result.gemoetry.location
+        console.log(`${process.argv[2]} is located at coordinates ${lat}, ${lng}, at the address ${response.json.result.formatted_address}.`)
+    }).catch(err => {
+        console.log(err)
+    })
   })
   .catch((err) => {
     console.log(err);
   });
-
-
-//import connection object from googleapi
--- 3_AM/index.js
+++ 3_AM/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname + "/View"));
 app.use(express.static(__dirname + "/Script"));
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+//sendFile takes absolute path
+
+app.get("/page1", (req, res) => {
+  res.sendFile(path.join(__dirname + "/page1.html"));
 });
 
 app.listen(3000);
